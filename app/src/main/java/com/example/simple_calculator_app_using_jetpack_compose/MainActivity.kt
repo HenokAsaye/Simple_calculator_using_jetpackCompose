14024bda387351f0c98a5c37818d1d1bf7db56e2
Henok Asaye <henokasaye77@gmail.com>
2025-04-09 15:34:12 +0300

    create the folder structure

---
+++ app/src/main/java/com/example/simple_calculator_app_using_jetpack_compose/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.simple_calculator_app_using_jetpack_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.simple_calculator_app_using_jetpack_compose.ui.theme.Simple_calculator_app_using_jetpack_composeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Simple_calculator_app_using_jetpack_composeTheme {
+            }
+        }
+    }
+}
+
