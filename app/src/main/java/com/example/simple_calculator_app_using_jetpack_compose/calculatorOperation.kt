bb647287ad901d0baed77d3306311e4d7900971a
Henok Asaye <henokasaye77@gmail.com>
2025-04-18 08:09:54 +0300

    create the objects on the claculator operation class

--- app/src/main/java/com/example/simple_calculator_app_using_jetpack_compose/calculatorOperation.kt
+++ app/src/main/java/com/example/simple_calculator_app_using_jetpack_compose/calculatorOperation.kt
@@ -1,4 +1,8 @@
 package com.example.simple_calculator_app_using_jetpack_compose
 
-class calculatorOperation {
+ sealed class calculatorOperation(val symbol:String) {
+     object Add: calculatorOperation("+")
+     object Subtract: calculatorOperation("-")
+     object Multiply: calculatorOperation("x")
+     object Divide: calculatorOperation("/")
 }
