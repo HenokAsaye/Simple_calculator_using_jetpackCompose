3598892585ad7ccd463139f9574669deeee0d6e2
Henok Asaye <henokasaye77@gmail.com>
2025-04-18 08:08:19 +0300

    file created for to handle the calculator state

--- app/src/main/java/com/example/simple_calculator_app_using_jetpack_compose/CalculatorState.kt
+++ app/src/main/java/com/example/simple_calculator_app_using_jetpack_compose/CalculatorState.kt
@@ -1,3 +1,10 @@
 package com.example.simple_calculator_app_using_jetpack_compose
 
-data class CalculatorState()
+data class CalculatorState(
+    val Number1:String = "",
+    val Number2:String = "",
+    val operation: calculatorOperation ?= null
+){
+
+}
+
