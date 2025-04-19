3b431d564be629249c55ae95893736cea5ca22cf
Henok Asaye <henokasaye77@gmail.com>
2025-04-19 08:13:17 +0300

    add the calculator composable in the mian activity functions

--- app/src/main/java/com/example/simple_calculator_app_using_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/example/simple_calculator_app_using_jetpack_compose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -13,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.simple_calculator_app_using_jetpack_compose.ui.theme.MediumGray
 import com.example.simple_calculator_app_using_jetpack_compose.ui.theme.Simple_calculator_app_using_jetpack_composeTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,6 +26,15 @@ class MainActivity : ComponentActivity() {
                 val viewModel = viewModel<CalculatorViewModel>()
                 val state = viewModel.state
                 val buttonSpacing = 8.dp
+                Calculator(
+                    state = state,
+                    onAction = viewModel::onAction,
+                    buttonSpacing = buttonSpacing,
+                   modifier =  Modifier
+                        .fillMaxSize()
+                        .background(MediumGray)
+                        .padding(16.dp)
+                )
             }
         }
     }
